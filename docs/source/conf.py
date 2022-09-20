8f13e8d75a57b3e5d54975a44626b690d703c30b
Jakub Cehak <jakub.cehak@medtronic.com>
2022-09-20 12:41:15 +0200

    Bump version: 0.2.0 → 1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ copyright = "2022, Jakub Cehak"
 author = "Jakub Cehak"
 
 # The short X.Y version
-version = "0.2.0"
+version = "1.0.0"
 # The full version, including alpha/beta/rc tags
 release = "0.1.0"
 
